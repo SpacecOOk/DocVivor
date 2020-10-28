7545a6c1cc69b3844171d476afd289182f8c34ec
Spacec00k <A01748632@itesm.mx>
2020-10-28 13:30:45 -0600

    Bruno hizo la clase Item

---
+++ core/src/mx/itesm/michel2/docvivor/Item.java
@@ -0,0 +1,33 @@
+package mx.itesm.michel2.docvivor;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Item extends Objeto{
+    private Animation<TextureRegion> animacion;
+    private float timerAnimacion;
+    private float x;
+    private float y;
+    public Item(Texture textura,float x, float y,int widthTextura,int HeightTextura){
+        this.x = x;
+        this.y = y;
+        TextureRegion region = new TextureRegion(textura);
+        TextureRegion[][] texturasFrame = region.split(widthTextura,HeightTextura);
+
+        //ANIMACION
+        TextureRegion[] arrFrames = {texturasFrame[0][0],texturasFrame[0][1],texturasFrame[0][2],texturasFrame[0][3],texturasFrame[0][4],texturasFrame[0][5],texturasFrame[0][6],texturasFrame[0][7],texturasFrame[0][8],texturasFrame[0][9]};
+        animacion = new Animation<TextureRegion>(0.1f,arrFrames);
+        animacion.setPlayMode(Animation.PlayMode.LOOP);
+        timerAnimacion = 0;
+    }
+    public void render(SpriteBatch batch){
+        float delta = Gdx.graphics.getDeltaTime();
+        timerAnimacion += delta;
+        TextureRegion frame = animacion.getKeyFrame(timerAnimacion);
+        batch.draw(frame,x,y);
+    }
+
+}
