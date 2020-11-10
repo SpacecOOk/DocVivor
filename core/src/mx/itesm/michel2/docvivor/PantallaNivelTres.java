ef9e017a4c91c39e246c74d86888efa71692e412
Bruno <A01748931@itesm.mx>
2020-11-09 18:34:48 -0600

    Agregue el fondo de nivel 3 y empece la implementacion del nivel3

--- core/src/mx/itesm/michel2/docvivor/PantallaNivelTres.java
+++ core/src/mx/itesm/michel2/docvivor/PantallaNivelTres.java
@@ -1,9 +1,13 @@
 package mx.itesm.michel2.docvivor;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 
 public class PantallaNivelTres extends Pantalla {
     private final Juego juego;
+
+    //Fondo
+    private Texture texturaFondoNivelTres;
 
 
     public PantallaNivelTres(Juego juego) {
@@ -12,12 +16,16 @@ public class PantallaNivelTres extends Pantalla {
 
     @Override
     public void show() {
-
+        texturaFondoNivelTres = new Texture("Level3/Level3_Background.png");
     }
 
     @Override
     public void render(float delta) {
-
+        borrarPantalla(0,0,05f);
+        batch.setProjectionMatrix(camara.combined);
+        batch.begin();
+        batch.draw(texturaFondoNivelTres,0,0);
+        batch.end();
     }
 
     @Override
