7981f6d11cfc07b4ca1078f2aa86cb5dce59f2f6
Spacec00k <A01748632@itesm.mx>
2020-11-27 14:48:13 -0600

    Ya esta el mail, falta arreglar el array metralleta

--- core/src/mx/itesm/michel2/docvivor/PantallaNivelTres.java
+++ core/src/mx/itesm/michel2/docvivor/PantallaNivelTres.java
@@ -234,14 +234,17 @@ public class PantallaNivelTres extends Pantalla {
         manager.load("Efectos_de_sonido/moneda.mp3", Sound.class);
         manager.load("Efectos_de_sonido/muerteDoc.mp3", Sound.class);
         manager.load("Efectos_de_sonido/saltoDoc.mp3", Sound.class);
+        manager.load("Efectos_de_sonido/disparo.mp3", Sound.class);
+        manager.load("Efectos_de_sonido/monster1.mp3", Sound.class);
+        manager.load("Efectos_de_sonido/sonidoMonstruo.mp3",Sound.class);
         manager.finishLoading();
         //Asignamos los sonidos a las variables
-        efectoDisparo = manager.get("Efectos_de_sonido/moneda.mp3");
+        efectoDisparo = manager.get("Efectos_de_sonido/disparo.mp3");
         efectoMuerte = manager.get("Efectos_de_sonido/muerteDoc.mp3");
-        efectoMuerteEnemigo = manager.get("Efectos_de_sonido/moneda.mp3");
+        efectoMuerteEnemigo = manager.get("Efectos_de_sonido/monster1.mp3");
         efectoPowerUp = manager.get("Efectos_de_sonido/moneda.mp3");
         efectoSalto = manager.get("Efectos_de_sonido/saltoDoc.mp3");
-        efectoMuerteEnemigoDos =manager.get("Efectos_de_sonido/moneda.mp3");
+        efectoMuerteEnemigoDos = manager.get("Efectos_de_sonido/sonidoMonstruo.mp3");
     }
 
     private void crearProyectil() {
